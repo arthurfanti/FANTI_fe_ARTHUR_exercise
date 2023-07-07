8c7d2ca63c0ffa03617ee7995e35eb824937e675
Arthur Fanti <arthur.fanti@hiplatform.com>
2023-07-07 09:43:42 -0300

    feat(features): extract router to own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
+import Router from 'components/Router';
 import * as React from 'react';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import TeamOverview from './pages/TeamOverview';
-import Teams from './pages/Teams';
-import UserOverview from './pages/UserOverview';
 
 const App = () => {
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            element: <Teams />,
-        },
-        {
-            path: '/team/:teamId',
-            element: <TeamOverview />,
-        },
-        {
-            path: '/user/:userId',
-            element: <UserOverview />,
-        },
-    ]);
-    return <RouterProvider router={router} />;
+    return <Router />;
 };
 
 export default App;
